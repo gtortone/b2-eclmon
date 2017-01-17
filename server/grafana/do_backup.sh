598f8ede366291e03bef9d31499b3c6a3451264d
Gennaro Tortone <gennaro.tortone@na.infn.it>
2017-01-17 18:42:10 +0100

    wizzy backup script for Grafana dashboards

---
+++ server/grafana/do_backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wizzy import orgs
+wizzy import dashboards
+wizzy import datasources
+
+echo "Password replaced with a fake one (changeme)..."
+for i in `grep --exclude=do_backup.sh -lR password *`; do sed -i 's|"password": "\(.*\)"|"password": "changeme"|' $i; done
+
+echo "Files changed:"
+grep --exclude=do_backup.sh -lR password *
