ad3abf1af27df1578611cd586762618ac0a8e244
Gennaro Tortone <gennaro.tortone@na.infn.it>
2017-02-06 10:50:13 +0900

    minor changes

--- server/grafana/do_backup.sh
+++ server/grafana/do_backup.sh
@@ -6,7 +6,7 @@ wizzy import dashboards
 wizzy import datasources
 
 echo "Password replaced with a fake one (changeme)..."
-for i in `grep --exclude=do_* -lR password *`; do sed -i 's|"password": "\(.*\)"|"password": "changeme"|' $i; done
+for i in `grep --exclude=do_*,conf/wizzy.json -lR password *`; do sed -i 's|"password": "u50p4all"|' $i; done
 
 echo "Files changed:"
 grep --exclude=do_backup.sh -lR password *
--- server/grafana/do_restore.sh
+++ server/grafana/do_restore.sh
@@ -6,9 +6,16 @@ read pwd
 echo "please press return to confirm or CTRL-C to exit"
 read
 
-for i in `grep --exclude=do_* -lR password *`; do sed -i "s|\"password\": \"\(.*\)\"|\"password\": \"$pwd\"|" $i; done
+for i in `grep --exclude=do_*,conf/wizzy.json -lR password *`; do sed -i "s|\"password\": \"\(.*\)\"|\"password\": \"$pwd\"|" $i; done
 
 wizzy init
 wizzy export org 1
 wizzy export dashboards
 wizzy export datasources
+
+if [ "$HOSTNAME" = usopmon01 ]; then
+   wizzy delete dashboard fuji-hall-ecl-endcap-monitoring-forward
+   wizzy delete dashboard fuji-hall-ecl-endcap-temperature-histogram
+   wizzy delete dashboard fuji-hall-ecl-endcap-relative-humidity-histogram
+   wizzy delete dashboard test
+fi
