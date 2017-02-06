5ba5079b52fe3e8db32f8d96d8de083f0019b560
Gennaro Tortone <gennaro.tortone@na.infn.it>
2017-02-06 11:06:20 +0900

    minor fix

--- server/grafana/do_backup.sh
+++ server/grafana/do_backup.sh
@@ -6,7 +6,7 @@ wizzy import dashboards
 wizzy import datasources
 
 echo "Password replaced with a fake one (changeme)..."
-for i in `grep --exclude=do_*,conf/wizzy.json -lR password *`; do sed -i 's|"password": "u50p4all"|' $i; done
+for i in `grep --exclude=do_*,conf/wizzy.json -lR password *`; do sed -i 's|"password": "changeme"|' $i; done
 
 echo "Files changed:"
 grep --exclude=do_backup.sh -lR password *
